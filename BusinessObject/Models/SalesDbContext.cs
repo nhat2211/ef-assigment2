e2c76fba0966560c57dd636e263256ef9f12e441
nhat2211 <tranquangnhat2211@gmail.com>
2023-10-20 09:00:35 +0700

    init project

---
+++ BusinessObject/Models/SalesDbContext.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessObject.Models;
+
+public partial class SalesDbContext : DbContext
+{
+    public SalesDbContext()
+    {
+    }
+
+    public SalesDbContext(DbContextOptions<SalesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Member> Members { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=(local);database=SalesDB;uid=sa;pwd=Keokeo2211(;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Member>(entity =>
+        {
+            entity.ToTable("MEMBER");
+
+            entity.Property(e => e.MemberId).HasColumnName("MEMBER_ID");
+            entity.Property(e => e.City)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("CITY");
+            entity.Property(e => e.CompanyName)
+                .HasMaxLength(40)
+                .IsUnicode(false)
+                .HasColumnName("COMPANY_NAME");
+            entity.Property(e => e.Country)
+                .HasMaxLength(15)
+                .IsUnicode(false)
+                .HasColumnName("COUNTRY");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("EMAIL");
+            entity.Property(e => e.Password)
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("PASSWORD");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.ToTable("ORDER");
+
+            entity.Property(e => e.OrderId)
+                .ValueGeneratedNever()
+                .HasColumnName("ORDER_ID");
+            entity.Property(e => e.Freight)
+                .HasColumnType("money")
+                .HasColumnName("FREIGHT");
+            entity.Property(e => e.MemberId).HasColumnName("MEMBER_ID");
+            entity.Property(e => e.OrderDate)
+                .HasColumnType("datetime")
+                .HasColumnName("ORDER_DATE");
+            entity.Property(e => e.RequiredDate)
+                .HasColumnType("datetime")
+                .HasColumnName("REQUIRED_DATE");
+            entity.Property(e => e.ShippedDate)
+                .HasColumnType("datetime")
+                .HasColumnName("SHIPPED_DATE");
+
+            entity.HasOne(d => d.Member).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.MemberId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ORDER_MEMBER_ID");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ProductId });
+
+            entity.ToTable("ORDER_DETAIL");
+
+            entity.Property(e => e.OrderId).HasColumnName("ORDER_ID");
+            entity.Property(e => e.ProductId).HasColumnName("PRODUCT_ID");
+            entity.Property(e => e.Discount).HasColumnName("DISCOUNT");
+            entity.Property(e => e.Quantity).HasColumnName("QUANTITY");
+            entity.Property(e => e.UnitPrice)
+                .HasColumnType("money")
+                .HasColumnName("UNIT_PRICE");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ORDER_DETAIL_ORDER");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ORDER_DETAIL_ORDER_PRODUCT");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("PRODUCT");
+
+            entity.Property(e => e.ProductId)
+                .ValueGeneratedNever()
+                .HasColumnName("PRODUCT_ID");
+            entity.Property(e => e.CategoryId).HasColumnName("CATEGORY_ID");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(40)
+                .IsUnicode(false)
+                .HasColumnName("PRODUCT_NAME");
+            entity.Property(e => e.UnitPrice)
+                .HasColumnType("money")
+                .HasColumnName("UNIT_PRICE");
+            entity.Property(e => e.UnitsInStock).HasColumnName("UNITS_IN_STOCK");
+            entity.Property(e => e.Weight)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("WEIGHT");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SalesWinApp/Main.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace SalesWinApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, toolStripMenuItem3 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "File";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(79, 24);
+            toolStripMenuItem1.Text = "Member";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(61, 24);
+            toolStripMenuItem2.Text = "Order";
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(74, 24);
+            toolStripMenuItem3.Text = "Product";
+            // 
+            // frmMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "frmMain";
+            Text = "Main";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+    }
+}
